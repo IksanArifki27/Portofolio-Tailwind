7b114178236a4004432e0c272666cd2ac556ac7c
iksan arifki <iksanarifki27@gmail.com>
2023-01-03 10:33:02 +0700

    fix footer

--- pages/index.js
+++ pages/index.js
@@ -242,7 +242,7 @@ export default function Home() {
       <footer>
         <div className="bg-gradient-to-tr from-teal-500 to-cyan-500 lg:w-full">
           <h2 className="text-white text-center p-3">
-            Create By Iksan arifki❤️&copy;2022
+            Create By Iksan arifki❤️
           </h2>
         </div>
       </footer>
